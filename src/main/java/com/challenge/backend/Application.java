347217495d9da17b7d0dc9027247b67a676cc8d8
Adrubal Castro <adrubalcastro@gmail.com>
2025-01-27 21:27:22 -0600

    first commit

---
+++ src/main/java/com/challenge/backend/Application.java
@@ -0,0 +1,19 @@
+package com.challenge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.challenge.backend.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "com.challenge.backend.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
